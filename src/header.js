9c59fc4821392af1c6cb1e3673d88778630b88ec
Floofies <fantasticdragons@gmail.com>
2018-11-04 01:16:56 -0400

    Renamed globals

--- src/header.js
+++ src/header.js
@@ -1,11 +1,11 @@
 var global = typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : typeof window !== 'undefined' ? window : {};
 if (!("tknLib" in global)) {
-	const TokenLib = require("./TokenLib.js");
-	global.tknLib = new TokenLib();
-	global.userLib = {};
-	for (const name in global.tknLib) global.userLib[name] = (...args) => global.tknLib[name].set(...args);
+	const hzTokenLib = require("./TokenLib.js");
+	global.hzTknLib = new hzTokenLib();
+	global.hzUserLib = {};
+	for (const name in global.hzTknLib) global.hzUserLib[name] = (...args) => global.hzTknLib[name].set(...args);
 }
-const tknLib = global.tknLib;
-const userLib = global.userLib;
-const Dispatcher = require("./Dispatcher.js");
-const hzDisp = new Dispatcher(tknLib);
+const hzTknLib = global.hzTknLib;
+const hzUserLib = global.hzUserLib;
+const hzDispatcher = require("./Dispatcher.js");
+const hzDisp = new hzDispatcher(hzTknLib);
