aa9749f27b0367ab90fffb77c764815f930d8fe0
Floofies <fantasticdragons@gmail.com>
2018-12-26 16:49:31 -0500

    Adding REPL

---
+++ bin/REPL.js
@@ -0,0 +1,25 @@
+// Sequential REPL
+const Dispatcher = require("../src/Dispatcher.js");
+const hzDisp = new Dispatcher();
+const hzCompile = require('hertzscript-compiler');
+process.stdout.write("> ");
+process.stdin.on("data", code => {
+	try {
+		var hzModule = new Function(
+			"hzUserLib",
+			"return " + hzCompile("(function (){" + code.toString() + "})", false, false, true) + ";"
+		);
+	} catch (error) {
+		console.error(error);
+		process.stdout.write("\n> ");
+		return;
+	}
+	try {
+		hzDisp.import(hzModule);
+		hzDisp.runComplete();
+	} catch (error) {
+		console.error(error);
+		hzDisp = new Dispatcher();
+	}
+	process.stdout.write("\n> ");
+});
