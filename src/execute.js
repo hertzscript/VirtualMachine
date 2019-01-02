7af76df28ae73c0b21340dd9c808e8ae7abf652c
Floofies <fantasticdragons@gmail.com>
2019-01-02 05:22:00 -0500

    Fixed globals

--- src/execute.js
+++ src/execute.js
@@ -1,12 +1,17 @@
 const Dispatcher = require("./Dispatcher.js");
+const hzCompile = require("hertzscript-compiler");
 module.exports = function execute(source, compile = false, spawn = false) {
-	if (compile) {
-		const hzCompile = require('hertzscript-compiler');
-		source = hzCompile(source, false, false, spawn);
-	}
-	source = "return hzUserLib.hookCoroutine(function*(){" + source + "});";
-	const hzModule = new Function("hzUserLib", source);
+	if (compile) source = hzCompile("(function (){" + source + "})", false, false, spawn);
+	else source = "(function (){" + source + "})"
+	var hzModule = new Function(
+		'exports',
+		'require',
+		'module',
+		'__filename',
+		'__dirname',
+		"return hzUserLib => { return " + source + "};"
+	);
 	const hzDisp = new Dispatcher();
-	hzDisp.import(hzModule);
+	hzDisp.import(hzModule(exports, require, module, __filename, __dirname));
 	return hzDisp.runComplete();
 };
