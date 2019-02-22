30aa4a733c4e8c0b3cbaa59fcbad16a550586ff8
Floofies <fantasticdragons@gmail.com>
2019-02-22 06:13:49 -0500

    Adding tokens namespace

--- src/lib/UserLib.js
+++ src/lib/UserLib.js
@@ -1,5 +1,5 @@
 function UserLib(tokenLib, detourLib) {
-	for (const name in tokenLib) this[name] = (...args) => tokenLib[name].set(args);
+	for (const name in tokenLib.tokens) this[name] = (...args) => tokenLib.tokens[name].set(args);
 	for (const name in detourLib) this[name] = (...args) => detourLib[name](...args);
 }
 module.exports = UserLib;
