006c2cd76580c48fcd84f9b4123d8c9cb1e1081e
Floofies <fantasticdragons@gmail.com>
2019-02-22 06:09:54 -0500

    Namespaced tokens

--- src/lib/TokenLib.js
+++ src/lib/TokenLib.js
@@ -10,90 +10,96 @@ function HzToken(type, ...argsArray) {
 }
 // Instruction Token library
 function TokenLib() {
-	// 2 Uniqueness Types: Only 1 instance each per-thread.
 	return {
+		// Returns true if a value is wrapped in a Kernelizer
+		isKernelized: input => ((typeof input) === "object") && (kernSym in input),
+		// 2 Uniqueness Types: Only 1 instance each per-thread.
 		// Type 1: Invocation Tokens,
 		//	Wrap userland functors and any operands needed to invoke them.
-		// Function call without arguments
-		call: new HzToken("call",
-			"functor",
-			"isTailCall"
-		),
-		// Function call with arguments
-		callArgs: new HzToken("callArgs",
-			"functor",
-			"args",
-			"isTailCall"
-		),
-		// Method call without arguments
-		callMethod: new HzToken("callMethod",
-			"object",
-			"property",
-			"isTailCall"
-		),
-		// Method call with arguments
-		callMethodArgs: new HzToken("callMethodArgs",
-			"object",
-			"property",
-			"args",
-			"isTailCall"
-		),
-		// NewExpression without arguments
-		new: new HzToken("new",
-			"functor"
-		),
-		// NewExpression with arguments
-		newArgs: new HzToken("newArgs",
-			"functor",
-			"args"
-		),
-		// NewExpression from method without arguments
-		newMethod: new HzToken("newMethod",
-			"object",
-			"property"
-		),
-		// NewExpression from method with arguments
-		newMethodArgs: new HzToken("newMethodArgs",
-			"object",
-			"property",
-			"args"
-		),
-		// SpawnExpression without arguments
-		spawn: new HzToken("spawn",
-			"functor"
-		),
-		// SpawnExpression with arguments
-		spawnArgs: new HzToken("spawnArgs",
-			"functor",
-			"args"
-		),
-		// SpawnExpression from method without arguments
-		spawnMethod: new HzToken("spawnMethod",
-			"object",
-			"property"
-		),
-		// SpawnExpression from method with arguments
-		spawnMethodArgs: new HzToken("spawnMethodArgs",
-			"object",
-			"property",
-			"args"
-		),
-		// Type 2: Data Tokens,
-		//	Wrap arbitrary userland datum when returning or yielding.
-		// ReturnStatement without an argument
-		return: new HzToken("return"),
-		// ReturnStatement with an argument
-		returnValue: new HzToken("returnValue",
-			"arg"
-		),
-		// YieldExpression without an argument
-		yield: new HzToken("yield"),
-		// YieldExpression with an argument
-		yieldValue: new HzToken("yieldValue",
-			"arg"
-		),
-		// Indicates an HZ-inserted loop yield
-		loopYield: new HzToken("loopYield"),
+		tokens: {
+			// Function call without arguments
+			call: new HzToken("call",
+				"functor",
+				"isTailCall"
+			),
+			// Function call with arguments
+			callArgs: new HzToken("callArgs",
+				"functor",
+				"args",
+				"isTailCall"
+			),
+			// Method call without arguments
+			callMethod: new HzToken("callMethod",
+				"object",
+				"property",
+				"isTailCall"
+			),
+			// Method call with arguments
+			callMethodArgs: new HzToken("callMethodArgs",
+				"object",
+				"property",
+				"args",
+				"isTailCall"
+			),
+			// NewExpression without arguments
+			new: new HzToken("new",
+				"functor"
+			),
+			// NewExpression with arguments
+			newArgs: new HzToken("newArgs",
+				"functor",
+				"args"
+			),
+			// NewExpression from method without arguments
+			newMethod: new HzToken("newMethod",
+				"object",
+				"property"
+			),
+			// NewExpression from method with arguments
+			newMethodArgs: new HzToken("newMethodArgs",
+				"object",
+				"property",
+				"args"
+			),
+			// SpawnExpression without arguments
+			spawn: new HzToken("spawn",
+				"functor"
+			),
+			// SpawnExpression with arguments
+			spawnArgs: new HzToken("spawnArgs",
+				"functor",
+				"args"
+			),
+			// SpawnExpression from method without arguments
+			spawnMethod: new HzToken("spawnMethod",
+				"object",
+				"property"
+			),
+			// SpawnExpression from method with arguments
+			spawnMethodArgs: new HzToken("spawnMethodArgs",
+				"object",
+				"property",
+				"args"
+			),
+			// Type 2: Data Tokens,
+			//	Wrap arbitrary userland datum when returning or yielding.
+			// ReturnStatement without an argument
+			return: new HzToken("return"),
+			// ReturnStatement with an argument
+			returnValue: new HzToken("returnValue",
+				"arg"
+			),
+			// YieldExpression without an argument (state object)
+			yield: new HzToken("yield",
+				"arg"
+			),
+			// YieldExpression with an argument (state object)
+			yieldValue: new HzToken("yieldValue",
+				"arg"
+			),
+			// Indicates an HZ-inserted loop yield
+			loopYield: new HzToken("loopYield"),
+		},
 		symbols: {
 			kernSym: kernSym,
 			// Property to access a HzFunctor instruction stream
@@ -102,8 +108,6 @@ function TokenLib() {
 			genSym: Symbol("Generator Symbol"),
 			// Marks an object as an iterator
 			iterSym: Symbol("Iterator Symbol"),
-			// Marks a functor as a subroutine.
-			srtSym: Symbol("Subroutine Symbol"),
 			// Marks a functor as a coroutine.
 			crtSym: Symbol("Coroutine Symbol"),
 			// Marks a functor as a constructor
