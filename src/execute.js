c4e06500686b88d01b88f797ffb6c60e08065779
Floofies <fantasticdragons@gmail.com>
2019-02-22 06:12:21 -0500

    Fixing wrapper

--- src/execute.js
+++ src/execute.js
@@ -1,8 +1,8 @@
 const Dispatcher = require("./Dispatcher.js");
 const hzCompile = require("hertzscript-compiler");
 module.exports = function execute(source, compile = false, spawn = false) {
-	if (compile) source = hzCompile("(function (){" + source + "})", false, false, spawn);
-	else source = "(function (){" + source + "})"
+	if (compile) source = hzCompile(source, false, false, spawn);
+	source = "(hzUserLib.hookCoroutine(function* (){" + source + "}))";
 	var hzModule = new Function(
 		'exports',
 		'require',
