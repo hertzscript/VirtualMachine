e2b678a2210b2ab18ff8b63fde5dd113d6316c9c
Floofies <fantasticdragons@gmail.com>
2019-03-09 01:58:58 -0500

    Fixing assignment typos

--- src/lib/UserLib.js
+++ src/lib/UserLib.js
@@ -1,5 +1,8 @@
 function UserLib(tokenLib, detourLib) {
 	for (const name in tokenLib.tokens) this[name] = (...args) => tokenLib.tokens[name].set(args);
+	this.return = tokenLib.tokens.return;
+	this.remit = tokenLib.tokens.remit;
+	this.loopYield = tokenLib.tokens.loopYield;
 	for (const name in detourLib) this[name] = (...args) => detourLib[name](...args);
 }
 module.exports = UserLib;
