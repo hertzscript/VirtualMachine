b2e225e7fa46353e1a80493775303c94c6a49c5e
Floofies <fantasticdragons@gmail.com>
2019-03-14 07:25:23 -0400

    Unsets props if they are missing

--- src/lib/Kernelizer.js
+++ src/lib/Kernelizer.js
@@ -5,11 +5,13 @@ function Kernelizer(...argsArray) {
 }
 Kernelizer.prototype.set = function (args) {
 	if (this.argSlots.length === 0) return this;
-	if (args.length === 0) return this.reset();
-	args.forEach((value, index) => this[this.argSlots[index]] = value);
+	this.argSlots.forEach((argName, index) => {
+		if (index <= args.length - 1) this[argName] = args[index];
+		else this[argName] = undefined;
+	});
 	return this;
 };
-Kernelizer.prototype.reset = function (resetValue = null) {
+Kernelizer.prototype.reset = function (resetValue) {
 	if (this.argSlots.length === 0) return this;
 	for (const argName of this.argSlots) this[argName] = resetValue;
 	return this;
